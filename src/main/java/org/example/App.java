356ea3da6a676b2fcd5b63a91571464bb70ba7f7
Berrybunny015 <berrybunny015@gmail.com>
2021-09-10 22:36:03 -0400

    additional code

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -18,28 +18,32 @@ public class App {
         Scanner orderAmountInput = new Scanner(System.in); //scan for the order amount
         double orderAmount = orderAmountInput.nextDouble();
 
-        System.out.println("\nWhat state do you live in (e.g. FL, WI, PA)? "); //ask the user for which state
+        System.out.println("\nWhat state do you live in (e.g. Wisconsin, Florida, Illinois)? "); //ask the user for which state
         Scanner stateInput = new Scanner(System.in); //scan for the state
         String state = stateInput.next();
 
         System.out.println("\nWhat county do you live in (e.g. Eau Claire, Dunn, Dane)? "); //ask the user for which county
         Scanner countyInput = new Scanner(System.in); //scan for the state
-        String county = countyInput.next();
+        String county = countyInput.nextLine();
 
         double taxAmount = 0; //the amount of taxes that will be added to the total cost in the end
 
         if (state.equals(wisconsin)) { //if the user is a resident of Wisconsin
-            taxAmount = orderAmount * wisconsinTax; //if so, then a new taxAmount is calculated
+
+            if (county.equals(eauClaire)) { //if the wisconsin resident lives in eau claire county
+                taxAmount = orderAmount * (wisconsinTax + eauClaireTax); //if so, then a new taxAmount is calculated
+            }
+            else if (county.equals(dunn)) { //if the wisconsin resident lives in dunn county
+                taxAmount = orderAmount * (wisconsinTax + dunnTax); //if so, then a new taxAmount is calculated
+            }
+            else { //if the wisconsin resident doesn't live in eau clair or dunn county
+                taxAmount = orderAmount * wisconsinTax; //if so, then a new taxAmount is calculated
+            }
+
             double totalCost = orderAmount + taxAmount; //calculate the total cost
 
-            if (county.equals(eauClaire)) {
-
-            }
-            else if (county.)
-
-            //output the subtotal, tax, and total cost
+            //output the total cost
             System.out.println("\nThe tax is $" + String.format("%.2f", taxAmount) + ".\n" + "The total is $" + String.format("%.2f", totalCost) + ".");
-
         }
         else if (state.equals(illinois)){ //if the user isn't a resident of Wisconsin
             taxAmount = orderAmount * illinoisTax; //if so, then a new taxAmount is calculated
